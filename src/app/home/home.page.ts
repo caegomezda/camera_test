edd30d4de45ac24594b20597200bd6643ba744e2
caegomezda <camiloedg23@gmail.com>
2021-04-21 13:15:52 -0500

    secod commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  image:string;
+  listaImage:any = [];
+  imageData:any
+  isImage:Boolean = false
+  constructor(private camera: Camera,
+              private webView: WebView,) {}
 
-  constructor() {}
+  takePictureFront() {
 
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    };
+    console.log("this.camera",this.camera);
+    console.log("this.image",this.image);
+    this.camera.getPicture(options)
+    .then((imageData) => {
+      this.image = this.webView.convertFileSrc(imageData);
+      this.imageData = imageData
+    }, (err) => {
+      console.log(err);
+    })
+  }
+
+  uploadImages(){
+    if (this.imageData) {
+      
+      this.listaImage.push(this.imageData);
+      this.isImage = true;
+
+    }else{
+      console.log("no hay imagen")
+    }
+  }
+  
 }
+
+
+
